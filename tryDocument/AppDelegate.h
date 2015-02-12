8a4c7bf667f8e03825e9cd04ca517ea5a190466f
赵晓行 <1414104973@qq.com>
2015-02-12 10:14:06 +0800

    Initial Commit

---
+++ tryDocument/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tryDocument
+//
+//  Created by apple on 15-2-12.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
